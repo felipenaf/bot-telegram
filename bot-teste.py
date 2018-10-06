b7911a2661ac3529b0d84c30142fae78e21e679d
Felipe <felipe.ferreira0192@gmail.com>
2018-10-06 01:12:05 -0300

    token no gitignore

--- bot-teste.py
+++ bot-teste.py
@@ -2,7 +2,6 @@ import os
 import telebot
 
 bot = telebot.TeleBot(os.environ['BOT_API_TOKEN'])
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, u"Olá, bem-vindo ao bot!")
