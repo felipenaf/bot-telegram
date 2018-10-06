c5a3308f449dc4e6ad42c758e99d1a98b3c5cdab
Felipe <felipe.ferreira0192@gmail.com>
2018-10-06 01:22:27 -0300

    start e help testados

--- bot-teste.py
+++ bot-teste.py
@@ -2,9 +2,13 @@ import os
 import telebot
 
 bot = telebot.TeleBot(os.environ['BOT_API_TOKEN'])
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, u"Olá, bem-vindo ao bot!")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, u"Esse bot foi desenvolvido por Felipe Ferreira!")
 
 #inicializa o bot
 bot.polling()
