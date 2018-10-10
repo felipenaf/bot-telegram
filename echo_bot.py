2c48a09a01b2eed630a85419b814b20b57fa4b1f
Felipe <felipe.ferreira0192@gmail.com>
2018-10-10 09:54:55 -0300

    inserido um array cumprimento inicial

--- echo_bot.py
+++ echo_bot.py
@@ -3,6 +3,10 @@ import time
 
 bot = telebot.TeleBot("698575115:AAHcK4l_xN6dwaYVXNtHiE66hXAldYibtAQ")
 
+def cumprimento_inicial():
+    cumprimento_inicial = ['ola', 'Ola', 'OLA', 'olá', 'Olá', 'OLÁ', 'oi', 'Oi', 'OI']
+    return cumprimento_inicial
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Olá, seja bem vindo")
@@ -13,8 +17,14 @@ def send_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+
+    ci = cumprimento_inicial()
     if message.text == "Tudo bem ?":
 	    bot.reply_to(message, "Td sim " + message.from_user.first_name + ", e vc?")
+
+    for msg in ci:
+        if message.text == msg:
+            bot.reply_to(message, "Olá " + message.from_user.first_name + ", :)")
 
 while True:
     try:
